409aa79ec60694407119cd20bcf0fa86399814a9
mednartem <mednartem@yandex.ru>
2021-01-18 20:59:57 +0300

    add file Game.js and fill it

---
+++ Game.js
@@ -0,0 +1,37 @@
+const nameMonster = "Dragon";
+const pointsOfHealthMonster = 100;
+const pointsOfDamageMonster = 10;
+const lifeMonstr = true;
+const nameUser = "Warrior";
+const pointsOfHealthUser = 100;
+const pointsOfDamageUser = 15;
+const life = true;
+const ally = false;
+const locationName = "Moscow";
+let remainingHealthMonster;
+let remainingHealthUser;
+
+console.log(
+    "name of the user: " + nameUser + ",",
+    "points of health: " + pointsOfHealthUser + ",",
+    "points of damage: " + pointsOfDamageUser + ",",
+    "life: " + life + ",",
+    "ally: " + ally
+);
+
+console.log(
+    "name of the monster: " + nameMonster + ",",
+    "points of health: " + pointsOfHealthMonster + ",",
+    "points of damage: " + pointsOfDamageMonster + ",",
+    "life: " + lifeMonstr
+);
+
+console.log(locationName);
+
+remainingHealthMonster = pointsOfHealthMonster - pointsOfDamageUser * 3;
+console.log("remaining health monster: " + remainingHealthMonster)
+
+remainingHealthUser = pointsOfHealthUser - pointsOfDamageMonster * 2;
+console.log("remaining health user: " + remainingHealthUser)
+
+
