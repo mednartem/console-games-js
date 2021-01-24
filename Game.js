5673ac241b4c8f1fd56b1eac2537be9c0e0b0bd9
mednartem <mednartem@yandex.ru>
2021-01-24 14:14:00 +0300

    change quote and change access to variable on

--- Game.js
+++ Game.js
@@ -1,41 +1,41 @@
-const nameMonster = "Dragon";
+const nameMonster = `Dragon`;
 const pointsOfHealthMonster = 100;
 const pointsOfDamageMonster = 10;
 const lifeMonstr = true;
-const nameUser = "Warrior";
+const nameUser = `Warrior`;
 const pointsOfHealthUser = 100;
 const pointsOfDamageUser = 15;
 const life = true;
 const ally = false;
-const locationName = "Moscow";
+const locationName = `Moscow`;
 let remainingHealthMonster;
 let remainingHealthUser;
 
 console.log(
-    "name of the user: " + nameUser + ",",
-    "points of health: " + pointsOfHealthUser + ",",
-    "points of damage: " + pointsOfDamageUser + ",",
-    "life: " + life + ",",
-    "ally: " + ally
+    `Name of the user: ` + `${nameUser}` + `,`,
+    `Points of health: ` + `${pointsOfHealthUser}` + `,`,
+    `Points of damage: ` + `${pointsOfDamageUser}` + `,`,
+    `Life: ` + `${life}` + `,`,
+    `Ally: ` + `${ally}`
 );
 
 console.log(
-    "name of the monster: " + nameMonster + ",",
-    "points of health: " + pointsOfHealthMonster + ",",
-    "points of damage: " + pointsOfDamageMonster + ",",
-    "life: " + lifeMonstr
+    `Name of the monster: ` + `${nameMonster}` + `,`,
+    `Points of health: ` + `${pointsOfHealthMonster}` + `,`,
+    `Points of damage: ` + `${pointsOfDamageMonster}` + `,`,
+    `Life: ` + `${lifeMonstr}`
 );
 
-console.log(locationName);
+console.log(`Location: ` + `${locationName}`);
 
-remainingHealthMonster = pointsOfHealthMonster - pointsOfDamageUser * 3;
-console.log("remaining health monster: " + remainingHealthMonster)
+remainingHealthMonster = `${pointsOfHealthMonster}` - `${pointsOfDamageUser}` * 3;
+console.log(`Remaining health monster: ` + `${remainingHealthMonster}`)
 
-remainingHealthUser = pointsOfHealthUser - pointsOfDamageMonster * 2;
-console.log("remaining health user: " + remainingHealthUser)
+remainingHealthUser = `${pointsOfHealthUser}` - `${pointsOfDamageMonster}` * 2;
+console.log(`Remaining health user: ` + `${remainingHealthUser}`)
 
 if(remainingHealthUser >= 0) {
-    console.log("user live, game to be continued")
+    console.log(`User live, the game to be continued`)
 } else {
-    console.log("game over")
+    console.log(`Game over`)
 }
