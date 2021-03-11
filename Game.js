6b648d46c50527e74bb47f60e964dc5aa581816c
mednartem <mednartem@yandex.ru>
2021-03-11 22:17:47 +0300

    remove excess code and cosmetic fix

--- Game.js
+++ Game.js
@@ -1,18 +1,5 @@
 const locationName = `Moscow`;
 
-function makeEntity(name, pointsOfHealth, pointsOfDamage, life, ally) {
-   return{
-        name : name,
-        pointsOfHealth: pointsOfHealth,
-        pointsOfDamage: pointsOfDamage,
-        life: life,
-        ally: ally
-   }
-}
-
-let user = makeEntity(`Warrior`, 100, getRandomInt(10), true, false);
-let monster = makeEntity(`Dragon`, 100, getRandomInt(10), true, false);
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
@@ -20,6 +7,19 @@ function getRandomInt(max) {
 function showMessage(message) {
     console.log(message);
 }
+
+function makeEntity(name, pointsOfDamage, pointsOfHealth = 100, life = true, ally = false) {
+    return {
+        name: name,
+        pointsOfDamage: pointsOfDamage,
+        pointsOfHealth: pointsOfHealth,
+        life: life,
+        ally: ally
+    }
+}
+
+let user = makeEntity(`Warrior`, getRandomInt(10));
+let monster = makeEntity(`Dragon`, getRandomInt(10));
 
 function attackToUser() {
     showMessage(`The monster attacks to user`)
@@ -30,46 +30,26 @@ function attackToUser() {
 function attackToMonster() {
     showMessage(`The user attacks to monster`)
     monster.pointsOfHealth -= user.pointsOfDamage;
-    showMessage(`Remaining health monster after attack: ${monster.pointsOfHealth}`);
+    showMessage(`Remaining health monster after attack: ${monster.pointsOfHealth}\n`);
 }
 
 function initialGameState() {
-    showMessage(
-        `Description of the user: \n` +
-        `   User name is ${user.name} \n` +
-        `   Points of health equals ${user.pointsOfHealth} \n` +
-        `   Points of damage equals ${user.pointsOfDamageUser} \n` +
-        `   Life is ${user.life} \n` +
-        `   Ally is ${user.ally} \n`
-    );
-
-    showMessage(
-        `Description of the monster: \n` +
-        `   Monster name is ${monster.name} \n` +
-        `   Points of health equals ${monster.pointsOfHealth} \n` +
-        `   Points of damage equals ${monster.pointsOfDamage} \n` +
-        `   Life is ${monster.life} \n` + 
-        `   Ally is ${monster.ally} \n`
-    );
-
-    showMessage(`Location is ${locationName} \n`);
+    console.log(user);
+    console.log(monster);
+    showMessage(`Location: ${locationName} \n`);
 }
 
 initialGameState();
-console.log(user);
-console.log(monster);
-
-while (user.pointsOfHealth || monster.pointsOfHealth <= 0) {
-    if (monster.pointsOfHealth >= 0 && user.pointsOfHealth >= 0) {
-        attackToMonster();
-        attackToUser();
-    } else {
+while (true) {
+    attackToUser();
+    attackToMonster();
+    if (user.pointsOfHealth <= 0 || monster.pointsOfHealth <= 0) {
         break;
     }
 }
 
 if (user.pointsOfHealth <= 0) {
-    showMessage(`\n Game over, The moster killed you`);
+    showMessage(`Game over, The moster killed you!`);
 } else if (monster.pointsOfHealth <= 0) {
-    showMessage(`\n Congratulations! You killed the monster`);
+    showMessage(`Congratulations! You killed the monster!`);
 }
