30596647ec4ea3dc56ac279250d29c799ac10114
mednartem <mednartem@yandex.ru>
2021-03-13 21:08:20 +0300

    fimprove game: fixing logging, fix logic while, now use boolean life

--- Game.js
+++ Game.js
@@ -1,67 +1,61 @@
 const locationName = `Moscow`;
-const monster = new Monster('Dragon', getRandomInt(1, 10));
-const user = new User('Warrior', getRandomInt(1, 10));
+const dragon = new MakeEntity('Dragon', getRandomInt(1, 10));
+// const ork = new Monster('Dragon', getRandomInt(1, 10));
+const warrior = new MakeEntity('Warrior', getRandomInt(1, 10));
+// const archer = new User('Archer', getRandomInt(1, 10));
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function User(name, pointsOfDamage, pointsOfHealth = 100, life = true, ally = false) {
+function MakeEntity(name, pointsOfDamage, pointsOfHealth = 100, life = true, ally = false) {
     this.name = name;
     this.pointsOfDamage = pointsOfDamage;
     this.pointsOfHealth = pointsOfHealth;
     this.life = life;
     this.ally = ally;
 
-    this.attackToMonster = function () {
-        console.log(`The user attacks to monster`)
-        console.log(`Damage monster: ${this.pointsOfDamage}`)
-        monster.pointsOfHealth -= this.pointsOfDamage;
+    this.attack = function () {
+        console.log(`The ${this.name} attacks`)
+        console.log(`Damage ${this.name}: ${this.pointsOfDamage}`)
     };
 
     this.showRemainingHealth = function () {
-        console.log(`Remaining health user: ${this.pointsOfHealth}\n`)
-    };
-}
-
-function Monster(name, pointsOfDamage, pointsOfHealth = 100, life = true, ally = false) {
-    this.name = name;
-    this.pointsOfDamage = pointsOfDamage;
-    this.pointsOfHealth = pointsOfHealth;
-    this.life = life;
-    this.ally = ally;
-
-    this.attackToUser = function () {
-        console.log(`The monster attacks to user`);
-        console.log(`Damage monster: ${this.pointsOfDamage}`);
-        user.pointsOfHealth -= this.pointsOfDamage;
-    };
-
-    this.showRemainingHealth = function () {
-        console.log(`Remaining health monster: ${this.pointsOfHealth}\n`);
+        console.log(`Remaining health ${this.name}: ${this.pointsOfHealth}\n`)
     };
 }
 
 function initialGameState() {
-    console.log(`USER \nName: ${user.name}`, `\nPoints of damage: ${user.pointsOfDamage}`, `\nPoints of health: ${user.pointsOfHealth}`, `\nLife: ${user.life}`, `\nAlly: ${user.ally}\n`);
-    console.log(`MONSTER \nName: ${monster.name}`, `\nPoints of damage: ${monster.pointsOfDamage}`, `\nPoints of health: ${monster.pointsOfHealth}`, `\nLife: ${monster.life}`, `\nÀlly: ${monster.ally}\n`);
+    console.log(warrior);
+    console.log(dragon);
     console.log(`Location: ${locationName} \n`);
 }
 
 initialGameState();
 
 while (true) {
-    monster.attackToUser();
-    user.showRemainingHealth();
-    user.attackToMonster();
-    monster.showRemainingHealth();
-    if (user.pointsOfHealth <= 0 || monster.pointsOfHealth <= 0) {
+    console.log(`____________START______________`)
+    dragon.attack();
+    warrior.pointsOfHealth -= dragon.pointsOfDamage;
+    warrior.showRemainingHealth();
+
+    if (warrior.pointsOfHealth <= 0) {
+        warrior.life = false;
         break;
     }
+    warrior.attack();
+    dragon.pointsOfHealth -= warrior.pointsOfDamage;
+    dragon.showRemainingHealth();
+
+    if (dragon.pointsOfHealth <= 0) {
+        dragon.life = false;
+        break;
+    }
+    console.log(`____________FINISH______________\n`)
 }
 
-if (user.pointsOfHealth <= 0) {
+if (!warrior.life) {
     console.log(`Game over, The moster killed you!`);
-} else if (monster.pointsOfHealth <= 0) {
+} else if (!dragon.life) {
     console.log(`Congratulations! You killed the monster!`);
 }
